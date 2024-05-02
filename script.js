f0b5d28f1871ad72e8d0021971dced69d8e0329e
jolene <sitienijolene@gmail.com>
2024-05-02 13:05:10 +0300

    add an async function for fetching data from the api

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const api = 'https://api-staging.adoptapet.com/search/search_form?key=hg4nsv85lp
 //grabbing the search button
 const searchButton = document.getElementById('search-button');
 //grabbing the input form(getting the search value of the type of dog)
-const input = document.querySelector('#search-input');
+let input = document.querySelector('#search-input');
 //grabbing area for result output
 const container = document.querySelector('.container');
+
+//function from fetching data from the api
+async function fetchPets(){
+    //fetcing the data from api
+    const response = await fetch(api);
+    //converting data to json
+    const petData = await response.json();
+}
